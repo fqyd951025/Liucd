74106ed2098eca6eea08d03c462215560f1e8560
fqyd_951025 <2420510778@qq.com>
2018-12-12 23:07:19 +0800

    12-12

--- js/routers/index.js
+++
@@ -1,8 +0,0 @@
-let exp = require('express');
-let router = exp.Router();
-
-router.use('/', require('./sign'));
-router.use('/', require('./login'));
-router.use('/', require('./response'));
-
-module.exports = router;
--- js/routers/sign.js
+++
@@ -1,8 +0,0 @@
-let exp = require('express');
-let router = exp.Router();
-
-router.get('/sign', function(req,res){
-	res.send('ok');
-});
-
-module.exports = router;
