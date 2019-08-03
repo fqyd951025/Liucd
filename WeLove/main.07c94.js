e73807f7999962a1768089f09eef7f38236ac651
fqyd951025 <2420510778@qq.com>
2019-08-03 16:40:52 +0800

    we2
    we2

--- WeLove/main.07c94.js
+++ WeLove/main.07c94.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.6940c.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.2359d.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.d738e.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.07c94.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -200,12 +200,12 @@ if (false) {
 else if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.f2e92.js');
+        require('src/settings.5823d.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.f2e92.js');
+        require('src/settings.5823d.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
